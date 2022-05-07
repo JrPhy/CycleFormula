11dfcaf3a736bd8dcc1ffd2804c4f9621a67a6d5
JrPhy <j0958322080@gmail.com>
2022-05-07 17:26:02 +0800

    for more accuate

--- assets/js/getData.js
+++ assets/js/getData.js
@@ -8,8 +8,11 @@ function Cal(form)
     const formElement = document.getElementById('form');
     const riderMass = Number(formElement[0].value);
     const bikeMass = Number(formElement[1].value);
-    const wantTime = formElement[2].value;
-    const route = Number(formElement[3].value);
+    const equipMass = Number(formElement[2].value);
+    const water = Number(formElement[3].value);
+    const food = Number(formElement[4].value);
+    const wantTime = formElement[5].value;
+    const route = Number(formElement[6].value);
     let duration = wantTime.split(':');
     timeSeg = Number(duration[0])*3600 + Number(duration[1])*60 + Number(duration[2]);
     
@@ -19,14 +22,14 @@ function Cal(form)
     {form.segment.value = "放行點 - 武嶺牌樓"}
     else if(route == 10)
     {form.segment.value = "Bedoin - Top"}
-    if(riderMass < 1 || bikeMass < 1 || timeSeg < 1)
+    if( riderMass < 1 || bikeMass < 1 || equipMass < 0 || water < 0 || food < 0 || timeSeg < 1)
     {
         form.WORK.value = "請輸入正確的數字";
         form.Pavg.value = "請輸入正確的數字";
     }
     else
     {
-        form.WORK.value = Module._work(riderMass, bikeMass, route, timeSeg);
+        form.WORK.value = Module._work(riderMass, bikeMass, equipMass, water, food, route, timeSeg);
         form.Pavg.value = (form.WORK.value*1000/timeSeg).toFixed(1);
         if (form.WORK.value == -2)
         {
